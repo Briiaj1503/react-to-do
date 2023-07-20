c08a75a7950e4d4fe3a2a0076b8d017b21154d4f
Briiaj1503 <britseidyaj@gmail.com>
2023-07-20 08:23:14 -0600

    Contador

--- src/Componentes/addList/index.jsx
+++ src/Componentes/addList/index.jsx
@@ -1,63 +1,75 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import CrearTarea from "../creartarea/index";
 import Tareas from "../tareas/index";
 
 const Agregartask = () => {
-    const [modal, setModal] = useState(false);
-    const [ListaTask, setListaTask] = useState([])
+  const [modal, setModal] = useState(false);
+  const [ListaTask, setListaTask] = useState([]);
+  const [tareasCheckeadas, setTareasCheckeadas] = useState(0); //contador de tareas useState
 
+  useEffect(() => {
+    let arr = localStorage.getItem("ListaTask");
 
-    useEffect (() => {
-        let arr = localStorage.getItem("ListaTask")
-       
-        if (arr) {
-            let obj = JSON.parse(arr)
-            setListaTask(obj)
-        }
-    },[])
+    if (arr) {
+      let obj = JSON.parse(arr);
+      setListaTask(obj);
+    }
+  }, []);
 
-
-
-    const BorrarTarea = (index) => {
-      let listT = ListaTask
-      listT.splice(index , 1)
-      localStorage.setItem("ListaTask", JSON.stringify(listT))
-      setListaTask(listT)
-      window.location.reload()
-    }  
+  useEffect(() => {
+    //funcion para tareas completadas, utilizando filter.
+    const completedTasks = ListaTask.filter((task) => task.completed);
+    setTareasCheckeadas(completedTasks.length);
     
-    const Check = (index) => {
-      const taskCopy = [...ListaTask];
-      taskCopy[index].completed = !taskCopy[index].completed
-      setListaTask(taskCopy);
-    }
-      
+    
+    localStorage.setItem("ListaTask", JSON.stringify(ListaTask));
+  }, [ListaTask]);
 
+  const BorrarTarea = (index) => {
+    let listT = ListaTask;
+    listT.splice(index, 1);
+    localStorage.setItem("ListaTask", JSON.stringify(listT));
+    setListaTask(listT);
+    window.location.reload();
+  };
 
-    const toggle = () => {
-        setModal(!modal);
-    }
+  const Check = (index) => {
+    const taskCopy = [...ListaTask];
+    taskCopy[index].completed = !taskCopy[index].completed;
+    setListaTask(taskCopy);
+  };
 
-    const GuardarTask = (taskObj) => {
-        let listT = ListaTask
-        listT.push(taskObj)
-        localStorage.setItem("ListaTask", JSON.stringify(listT))
-        setListaTask(listT)
-        setModal(false)
-        setListaTask(ListaTask)
+  const toggle = () => {
+    setModal(!modal);
+  };
 
-    }
+  const GuardarTask = (taskObj) => {
+    let listT = [...ListaTask, taskObj];
+    setListaTask(listT);
+    setModal(false);
+  };
+
   return (
     <>
       <div className="header text-center">
         <h3>Lista de Tareas</h3>
-        <button className="btn btn-danger mt-2"onClick={() => setModal(true)}>Crear Tarea</button>
+        <button className="btn btn-danger mt-2" onClick={() => setModal(true)}>Crear Tarea</button>
       </div>
       <div className="contenedor-tarea">
-        {ListaTask && ListaTask.map((obj , index) => <Tareas taskObj = {obj} index = {index} BorrarTarea = {BorrarTarea} Check={Check}/> )}
-
+        {ListaTask && ListaTask.map((obj, index) => (
+          <Tareas
+            key={index}
+            taskObj={obj}
+            index={index}
+            BorrarTarea={BorrarTarea}
+            Check={Check}
+          />
+        ))}
       </div>
-      <CrearTarea toggle={toggle} modal={modal} save = {GuardarTask}/>
+      <div className="contador-tareas-checkeadas">
+        <h4>Tareas Completadas: {tareasCheckeadas}</h4>
+      </div>
+      <CrearTarea toggle={toggle} modal={modal} save={GuardarTask} />
     </>
   );
 };
